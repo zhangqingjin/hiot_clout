b5e17bc3011bda57c9e666e471d51d9c73cabc43
zhangqingjin <450702796@qq.com>
2020-05-02 15:46:38 +0800

    3.1:添加OkHttp测试

--- app/src/main/java/com/example/hiot_clout/test/networktest/TestOkHttpActivity.java
+++ app/src/main/java/com/example/hiot_clout/test/networktest/TestOkHttpActivity.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -24,7 +25,8 @@ import okhttp3.Response;
  */
 public class TestOkHttpActivity extends AppCompatActivity {
 
-    private static final String basUrl = "http://163.177.151.109";
+//    private static final String basUrl = "http://163.177.151.109";
+    private static final String basUrl = "http://114.67.88.191:8080";
     private static final String TAG = "TestOkHttpActivity";
 
     @Override
@@ -48,6 +50,89 @@ public class TestOkHttpActivity extends AppCompatActivity {
                 testEnqueue();
             }
         });
+        //登录测试
+        final Button btnLogin = findViewById(R.id.btn_okhttp_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login("zhangdaxia","abc123","app");
+            }
+        });
+        //获取用户信息
+        Button btnGetUserInfo = findViewById(R.id.btn_okhttp_userinfo);
+        btnGetUserInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               getUserInfo("cd23a7a9afb2473895a6741b5090f5f6_b849bbd2feb847a5ba6386e68ccde53b_use");
+
+            }
+        });
+        //修改邮箱
+        Button btnUpdataEmail = findViewById(R.id.btn_okhttp_updata_email);
+        btnUpdataEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updataEmail("cd23a7a9afb2473895a6741b5090f5f6_b849bbd2feb847a5ba6386e68ccde53b_use","zhangtian@qq.com");
+
+            }
+        });
+
+    }
+
+    /**
+     * 修改邮箱
+     * @param authorization
+     * @param email
+     */
+    private void updataEmail(String authorization, String email) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        FormBody body = new FormBody.Builder().build();
+        String url =basUrl + "/user/email?email="+ email;
+        Request request = new Request.Builder().put(body).url(url)
+                .header("Authorization",authorization).build();
+        callEnqueue(okHttpClient, request);
+    }
+
+    private void callEnqueue(OkHttpClient okHttpClient, Request request) {
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.e(TAG, "onFailure: " + e.getMessage(), e);
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d(TAG, "onResponse: " + response.body().string());
+            }
+        });
+    }
+
+    /**
+     * 获取用户信息
+     * @param authorization
+     */
+    private void getUserInfo(String authorization) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        FormBody body = new FormBody.Builder().build();
+        String url =basUrl + "/user/one";
+        Request request = new Request.Builder().get().url(url)
+                .header("Authorization",authorization).build();
+        callEnqueue(okHttpClient, request);
+    }
+
+    /**
+     *登录
+     * @param userName
+     * @param password
+     * @param loginCode
+     */
+    private void login(String userName, String password, String loginCode) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        FormBody body = new FormBody.Builder().build();
+        String url =basUrl + String.format("/auth/login?username=%s&password=%s&loginCode=%s",
+                userName,password,loginCode);
+        Request request = new Request.Builder().post(body).url(url).build();
+        callEnqueue(okHttpClient, request);
     }
 
 
@@ -73,16 +158,6 @@ public class TestOkHttpActivity extends AppCompatActivity {
     private void testEnqueue() {
         OkHttpClient okHttpClient = new OkHttpClient();
         Request request = new Request.Builder().url(basUrl).build();
-        okHttpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                Log.e(TAG, "onFailure: "+ e.getMessage(), e );
-            }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                Log.d(TAG, "onResponse: " + response.body().string());
-            }
-        });
+        callEnqueue(okHttpClient, request);
     }
 }
